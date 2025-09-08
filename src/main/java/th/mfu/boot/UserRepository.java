f4c02f335647e662f53fd8ca4c169fcaa98a8cf8
Chaiyan007 <6731503007@lamduan.mfu.ac.th>
2025-09-08 11:43:59 +0000

    Chaiyan_6731503007

--- src/main/java/th/mfu/boot/UserRepository.java
+++ src/main/java/th/mfu/boot/UserRepository.java
@@ -1,9 +1,11 @@
 package th.mfu.boot;
 
 import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    List<User> findAll();
+
+    User findByUsername(String username);
 }
